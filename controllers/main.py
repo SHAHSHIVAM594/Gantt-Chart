57dfa2fd30bb4f68774afcd10cf44192e11d7198
SHAHSHIVAM594 <40867350+SHAHSHIVAM594@users.noreply.github.com>
2019-10-19 23:05:42 +0530

    remove extra space and make code proper

--- controllers/main.py
+++ controllers/main.py
@@ -3,15 +3,14 @@
 from odoo import http
 from odoo.http import request
 
-
 class WebsiteDepartment(http.Controller):
-
     @http.route("/authors/search", type="json", auth="public")
     def all_authors(self, author):
         Authors = request.env['json.model'].search([('author', '=', author)])
         mylist = []
         for rec in Authors:
-            mylist.append({
+            mylist.append
+            ({
                 'author': rec.author,
                 'taskName': rec.name,
                 'types': rec.types,
